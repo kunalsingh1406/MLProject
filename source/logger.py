90e9ae7d628273f2cf4025a0e1ce5e7eb81016f7
kunalsingh1406 <kunal964121@gmail.com>
2025-08-23 14:37:33 +0530

    Data Ingestion

--- source/logger.py
+++ source/logger.py
@@ -13,6 +13,7 @@ logging.basicConfig(
     filename=LOG_FILE_PATH,
     format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
+    force=True
 )
 
 if __name__ == "__main__":
